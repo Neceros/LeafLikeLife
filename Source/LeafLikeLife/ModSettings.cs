526f5f3dc811d9ffa8a6e22ec61af944ddce8ef3
Neceros <neceros@gmail.com>
2020-04-13 14:56:03 -0700

    Added settings

---
+++ Source/LeafLikeLife/ModSettings.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace LeafLikeLife
+{
+  public class LLLModSettings : ModSettings
+  {
+    public static float amountPenaltyConsciousness = -0.15f;
+
+
+    public override void ExposeData()
+    {
+      base.ExposeData();
+      Scribe_Values.Look(ref amountPenaltyConsciousness, "LLLamountPenaltyConsciousness");
+    }
+  }
+
+  public class LLLMod : Mod
+  {
+    LLLModSettings settings;
+    public LLLMod(ModContentPack con) : base(con)
+    {
+      this.settings = GetSettings<LLLModSettings>();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+      Listing_Standard listing = new Listing_Standard();
+      listing.Begin(inRect);
+      listing.Label("LLLConsciousnessLabel".Translate() + ": " + (LLLModSettings.amountPenaltyConsciousness * 100) + "%", tooltip: "LLLConsciousnessTooltip".Translate());
+      LLLModSettings.amountPenaltyConsciousness = listing.Slider(RoundToNearestTenth(LLLModSettings.amountPenaltyConsciousness), -0f, -1f);
+      listing.End();
+      base.DoSettingsWindowContents(inRect);
+    }
+
+    public override void WriteSettings()
+    {
+      LLLUtility.UpdateConsciousnessPenalty();
+
+      base.WriteSettings();
+    }
+
+    public override string SettingsCategory()
+    {
+      return "LLLTitle".Translate();
+    }
+
+    private float RoundToNearestHalf(float val)
+    { 
+      return (float)Math.Round(val * 2, MidpointRounding.AwayFromZero) / 2;
+    }
+
+    private float RoundToNearestTenth(float val)
+    {
+      return (float)Math.Round(val * 100, MidpointRounding.AwayFromZero) / 100;
+    }
+  }
+}
---
+++ Source/LeafLikeLife/StaticConstructor.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace LeafLikeLife
+{
+  [StaticConstructorOnStartup]
+  public static class LLLStartUp
+  {
+    static LLLStartUp()
+    {
+      // Loads right before main menu
+      LLLUtility.UpdateConsciousnessPenalty();
+    }
+  }
+
+  public class LLLUtility
+  {
+    public static void UpdateConsciousnessPenalty()
+    {
+      //DefDatabase<HediffDef>.GetNamed("SmokeleafHigh").stages[0].capMods[0].offset = LLLModSettings.amountPenaltyConsciousness;
+      HediffDef.Named("SmokeleafHigh").
+        stages.Where((HediffStage stage) => stage.capMods.Any((PawnCapacityModifier mod) => mod.capacity == PawnCapacityDefOf.Consciousness)).First().
+        capMods.Where((PawnCapacityModifier mod) => mod.capacity == PawnCapacityDefOf.Consciousness).First().offset = LLLModSettings.amountPenaltyConsciousness;
+    }
+  }
+}
